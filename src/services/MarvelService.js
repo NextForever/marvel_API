a7271b9db002de782ecb7c4b37ad93614feca3c1
GezMin <s.shvetsov.v@gmail.com>
2023-01-11 02:27:51 +0600

    getCharasters randomChar

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -11,12 +11,25 @@ class MarvelService {
         return await res.json();
     };
 
-    getAllCharasters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=841&${this._apiKey}`);
+    getAllCharasters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=841&${this._apiKey}`);
+        return res.data.results.map(this._transformCharaster);
     };
 
-    getCharasters = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharaster = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharaster(res.data.results[0]);
+    };
+
+    _transformCharaster = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            decription: char.description.length > 190 ? char.description.substring(0, 190) + "..." : "No decription",
+            thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        };
     };
 }
 
