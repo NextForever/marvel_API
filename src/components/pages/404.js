d37046d487dc4874b3286f56ceea1f6fd1b838d9
GezMin <s.shvetsov.v@gmail.com>
2023-01-29 22:03:23 +0600

    genarate meta page

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,27 +1,36 @@
-import React from "react";
-import ErrorMessage from "../errorMessage/ErrorMessage";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import { Link } from 'react-router-dom';
 
 const Page404 = () => {
     return (
-        <div>
+        <>
+            <Helmet>
+                <meta name='description' content='404 page found' />
+                <title>404 page found</title>
+            </Helmet>
             <div>
-                <ErrorMessage />
-                <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>Page doesn't exist</p>
-                <Link
-                    style={{
-                        display: "block",
-                        textAlign: "center",
-                        fontWeight: "bold",
-                        fontSize: "24px",
-                        marginTop: "30px",
-                    }}
-                    to='/'
-                >
-                    Back to main page
-                </Link>
+                <div>
+                    <ErrorMessage />
+                    <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>
+                        Page doesn't exist
+                    </p>
+                    <Link
+                        style={{
+                            display: 'block',
+                            textAlign: 'center',
+                            fontWeight: 'bold',
+                            fontSize: '24px',
+                            marginTop: '30px',
+                        }}
+                        to='/'
+                    >
+                        Back to main page
+                    </Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,16 +1,23 @@
+import { Helmet } from 'react-helmet';
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({ data }) => {
     const { name, description, thumbnail } = data;
 
     return (
-        <div className='single-comic'>
-            <img src={thumbnail} alt={name} className='single-comic__char-img' />
-            <div className='single-comic__info'>
-                <h2 className='single-comic__name'>{name}</h2>
-                <p className='single-comic__descr'>{description}</p>
+        <>
+            <Helmet>
+                <meta name='description' content='Page with list of our comics' />
+                <title>{name}</title>
+            </Helmet>
+            <div className='single-comic'>
+                <img src={thumbnail} alt={name} className='single-comic__char-img' />
+                <div className='single-comic__info'>
+                    <h2 className='single-comic__name'>{name}</h2>
+                    <p className='single-comic__descr'>{description}</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
